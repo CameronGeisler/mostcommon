107bceb1807c4f2af8200e00773eca97e7785d0c
Cameron Geisler <ckg888888@gmail.com>
2023-04-28 19:45:56 -0700

    Added callback.html file for redirectURi

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 document.getElementById('login-btn').addEventListener('click', () => {
     // Replace with your own Instagram App ID and redirect URI
     const appId = '2011593365861809';
-    const redirectUri = '"https://camerongeisler.github.io/mostcommon/instagram/callback';
+    const redirectUri = '"https://camerongeisler.github.io/mostcommon/callback.html';
     const responseType = 'token';
     const scope = 'user_profile,user_media';
 
@@ -90,7 +90,7 @@ const tokenUrl = "https://api.instagram.com/oauth/access_token/";
 
 // Set up the Instagram App ID and Redirect URI
 const appId = "2011593365861809";
-const redirectUri = "https://camerongeisler.github.io/mostcommon/instagram/callback";
+const redirectUri = "https://camerongeisler.github.io/mostcommon/callback.html";
 const deauthCallbackUrl = "https://camerongeisler.github.io/mostcommon/instagram/deauthorize"; // Replace with your own Deauthorize Callback URL
 
 // Set up the Instagram API endpoints
